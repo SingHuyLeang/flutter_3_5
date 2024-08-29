9b86a1dfffa5e4ea1e2fa2b89d333a72b4a4ff63
SingHuyLeang <leang6525@gmail.com>
2024-08-29 16:43:54 +0700

    build pop back and image

--- lib/ui/components/card.dart
+++ lib/ui/components/card.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:shoes_app/data/product.dart';
 import 'package:shoes_app/ui/components/widgets/text.dart';
--- lib/ui/screens/detail.dart
+++ lib/ui/screens/detail.dart
@@ -9,12 +9,47 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-         title: SText(text: product.name),
-      ),
-      body: Center(
-        child: Image.asset(product.image),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 400,
+              padding: const EdgeInsets.only(top: 24),
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(26),
+                  bottomRight: Radius.circular(26),
+                ),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    product.image,
+                  ),
+                ),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: CircleAvatar(
+                      backgroundColor: Colors.grey[300],
+                      child: Icon(Icons.adaptive.arrow_back),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.shopping_cart),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
