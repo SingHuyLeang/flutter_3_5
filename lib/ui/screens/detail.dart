f977259e1119255da3dc30637afbc8d80cd1f864
SingHuyLeang <leang6525@gmail.com>
2024-09-02 16:13:06 +0700

    detail-more

--- lib/ui/screens/detail.dart
+++ lib/ui/screens/detail.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:shoes_app/data/product.dart';
 import 'package:shoes_app/ui/components/widgets/text.dart';
+import 'package:shoes_app/ui/theme/colors.dart';
+import 'package:shoes_app/ui/theme/type.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key, required this.product});
 
   final Product product;
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  int rating = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +35,7 @@ class DetailScreen extends StatelessWidget {
                 image: DecorationImage(
                   fit: BoxFit.cover,
                   image: AssetImage(
-                    product.image,
+                    widget.product.image,
                   ),
                 ),
               ),
@@ -48,6 +57,116 @@ class DetailScreen extends StatelessWidget {
                 ],
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        flex: 4,
+                        child: SText(
+                          text: widget.product.name,
+                          size: 24,
+                          font: Font.bold,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Row(
+                          children: List.generate(
+                            5,
+                            (index) => GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  rating = index + 1; // 3 point
+                                });
+                              },
+                              child: Icon(
+                                Icons.star,
+                                color: rating > index
+                                    ? SColors.primary
+                                    : Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 24),
+                  SText(
+                    text: "\$ ${widget.product.price}",
+                    size: 20,
+                    font: Font.bold,
+                  ),
+                  const SizedBox(height: 24),
+                  SText(
+                    text: widget.product.description,
+                    size: 16,
+                  ),
+                  const SizedBox(height: 24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SText(text: "Color", font: Font.bold, size: 20),
+                              SizedBox(height: 16),
+                              Row(
+                                children: List.generate(
+                                  widget.product.colors.length,
+                                  (index) => GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        //
+                                      });
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: CircleAvatar(
+                                        radius: 16,
+                                        backgroundColor:
+                                            widget.product.colors[index],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )),
+                      Expanded(
+                        flex: 1,
+                        child: Row(
+                          children: List.generate(
+                            5,
+                            (index) => GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  rating = index + 1; // 3 point
+                                });
+                              },
+                              child: Icon(
+                                Icons.star,
+                                color: rating > index
+                                    ? SColors.primary
+                                    : Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
